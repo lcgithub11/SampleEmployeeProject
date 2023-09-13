133cccd996ca91c217014e6fd582f287a456d754
lcgithub11 <lcgithub1@gmail.com>
2023-09-13 18:30:58 +0300

    Add project files.

---
+++ SampleEmployeeProject/Model/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public bool IsActive { get; set; } = true;
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+    public DateTime? UpdatedDate { get; set; }
+    public string CreatedBy { get; set; } = "System";
+    public string UpdatedBy { get; set; }
+}
---
+++ SampleEmployeeProject/Model/FamilyMember.cs
@@ -0,0 +1,15 @@
+﻿public class FamilyMember : BaseEntity
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public DateTime Birthdate { get; set; }
+    public string PhoneNumber { get; set; }
+    public Guid EmployeeId { get; set; }
+    public FamilyType Type { get; set; } // Enum: Spouse, Child
+}
+
+public enum FamilyType
+{
+    Spouse,
+    Child
+}
